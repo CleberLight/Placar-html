adc3e4dec10dfe2ae5012f8702212ffb2395c52a
Cleber Azevedo <75389367+CleberLight@users.noreply.github.com>
2022-06-02 20:40:14 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,30 @@
+video[1] = "Torre de Copos.mov";
+video[2] = "Bolas no Pote.mov";
+video[3] = "Gol.mov";
+video[4] = "Tiro no Come.mov";
+video[5] = "Arco no Cone.mov";
+video[6] = "Avião.mov";
+video[7] = "Balão e Copos.mov";
+video[8] = "Bola na Colher.mov";
+video[9] = "Parafusos.mov";
+video[10] = "Tiro no CD.mov";
+imagem = "logo_pre_oficial_2.png";
+som = "AUD-20141031-WA0003.mp3";
+
+/*video[1] = "VID-20150522-WA0000.mp4";
+video[2] = "VID-20150425-WA0000.mp4";
+video[3] = "VID-20150514-WA0003.mp4";*/
+
+/* Array
+//construção simples sem dimensionamento
+meuVetor1 = new Array();
+
+//construção simples com dimensionamento
+meuVetor2 = new Array(4);
+
+//construção inserindo valores
+meuVetor3 = new Array ("João", "Roberto", "José", "Maria");
+
+meuVetor2[1] = 540;
+meuVetor2[3] = 8456;
+*/
